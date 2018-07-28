eaf7ce3ea5847969d9aa8a3c1507ba951b823a72
Anc0 <252eur@gmail.com>
2018-07-28 07:56:14 +0200

    Added supervisor conf for mqtt worker.

--- fabfile.py
+++ fabfile.py
@@ -7,15 +7,24 @@ env.user = 'pi'
 env.hosts = [production_pi]
 
 @task
-def production_config():
+def production_api():
     env.remote_dir = '/home/pi/home-temp-restapi/source/'
     env.local_dir = '/home/andraz/Projects/home_temp_restapi/'
+    env.supervisor = 'home-temp-restapi.production.conf'
+    env.nginx = 'home-temp-restapi.production.conf'
+
+@task
+def production_mqtt_worker():
+    env.remote_dir = '/home/pi/home-temp-restapi/source/'
+    env.local_dir = '/home/andraz/Projects/home_temp_restapi/'
+    env.supervisor = 'mqqt-worker.production.conf'
+    env.nginx = 'home-temp-restapi.production.conf'
 
 @task
 def deploy(requirements=False, supervisor=False, nginx=False):
     """
-    Take local project, upload it to the server with the right configuration and
-    do some restart configuration magic with supervisor, gunicorn and nginx.
+    Take local project, upload it to the server with the right configuration. Then check flags and install requirements,
+    update supervisor configuration and update nginx configuration.
     """
     rsync_project(remote_dir=env.remote_dir, local_dir=env.local_dir)
 
@@ -24,10 +33,10 @@ def deploy(requirements=False, supervisor=False, nginx=False):
             run('pip install -r ~/home-temp-restapi/source/requirements.txt')
 
     if supervisor:
-        sudo('cp /home/pi/home-temp-restapi/source/conf/supervisor.home-temp-restapi.production.conf /etc/supervisor/conf.d')
+        sudo('cp /home/pi/home-temp-restapi/source/conf/supervisor.{} /etc/supervisor/conf.d'.format(env.supervisor))
         sudo('supervisorctl reread')
         sudo('supervisorctl update')
 
     if nginx:
-        sudo('cp /home/pi/home-temp-restapi/source/conf/nginx.home-temp-restapi.production.conf /etc/nginx/')
+        sudo('cp /home/pi/home-temp-restapi/source/conf/nginx.{} /etc/nginx/'.format(env.nginx))
         sudo('nginx -s reload')
